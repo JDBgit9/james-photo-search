38701b1258cb95b878dbf9d89436672c0df38c0f
JDBgit9 <info@jamesdbeeks.com>
2021-01-21 11:14:48 -0500

    added the api key

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import "./Home.css";
+import $ from "jquery";
 
 function Home() {
     const [input, setInput]=useState("")
@@ -8,6 +9,9 @@ function Home() {
     const handleSubmit=(e)=>{
         e.preventDefault()
         console.log(input)
+        $.ajax(`https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=${flickrapi}&tags=${input}`).done(response=>{
+            console.log(response)
+        })
     }
     return (
         <div className="home">
