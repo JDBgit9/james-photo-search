df6b929b4b20e2f26c9b258c389d110364ed043a
JDBgit9 <info@jamesdbeeks.com>
2021-01-27 08:25:07 -0500

    changed the heaiht of the home and added a footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./Home";
+import Footer from "./Footer"
 
 function App() {
   return (
     <div className="App">
      <Home />
+     <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+
+        <div className="footer">
+            <p>James Beeks GitHub: JDBgit9 email: jdbwebdev@protonmail.com. &copy; 2021</p>
+        </div>
+    )
+}
+
+export default Footer
